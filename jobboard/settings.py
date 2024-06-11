9ff06fe0a6939cc6af0fd3483d83bcc67e9fd0ce
rhbanks779 <rhbanks779@gmail.com>
2024-06-11 12:14:39 -0400

    procfile

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -62,9 +62,9 @@ ROOT_URLCONF = 'jobboard.urls'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR / "static"),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
